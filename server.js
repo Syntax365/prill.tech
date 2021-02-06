608f0e7fee56cf2e0b590f9fb37bf75531c86b46
Syntax365 <synxtax365@gmail.com>
2021-02-06 15:31:29 -0500

    Node Modules

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-const express = require('express');
+import express from 'express';
 const PORT = process.env.HTTP_PORT || 4001;
 const app = express();
 app.get('/', (req, res) => {
-  res.send('flowers smell nice');
+  res.send('just gonna send it');
+});
+app.get('/flower', (req, res) => {
+  res.json({
+    name: 'Dandelion',
+    colour: 'Blue-ish'
+  });
 });
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}.`);
