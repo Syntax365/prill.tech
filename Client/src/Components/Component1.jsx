6b76ee0175224f707da453c792bf935da9cf4c66
Syntax365 <synxtax365@gmail.com>
2021-02-11 20:04:04 -0500

    Add First Component, Structure for Multipage

---
+++ Client/src/Components/Component1.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Component1 extends Component {
+  render() {
+    return (
+      <div>
+        <h3>This is a Component1 component.</h3>
+      </div>
+    );
+  }
+}
+
+export default Component1;
--- Client/src/Pages/Homepage/App.js
+++ Client/src/Pages/Homepage/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+
+import Component1 from '../../Components/Component1.jsx'
+
 import './App.css';
+
 class App extends Component {
+
   constructor(props) {
+
     super(props);
     this.state = {
       testResponse: {}
     }
+
     this.getTestData();
+
   }
+
   getTestData() {
     fetch('/test')
       .then(response => response.json())
@@ -17,13 +26,18 @@ class App extends Component {
         });
       });
   }
+
   render() {
     return (
-      <div className="App">
-        <h1>{this.state.testResponse.fullName}</h1>
-        <p>Test Response: {this.state.testResponse.integrationTest}</p>
-      </div>
+      
+        <div className="App">
+          <h1>{this.state.testResponse.fullName}</h1>
+          <p>Test Response: {this.state.testResponse.integrationTest}</p>
+          <Component1 />
+        </div>
+      
     );
   }
 }
+
 export default App;
