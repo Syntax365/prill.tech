5b64c26d23cd689dc724c6fd5edc286cda4b6ab9
Syntax365 <syntax365@gmail.com>
2021-02-21 19:00:00 -0500

    Multi Device Source Control

--- Client/src/Pages/Homepage/Hero/HomepageHero.jsx
+++ Client/src/Pages/Homepage/Hero/HomepageHero.jsx
@@ -15,6 +15,7 @@ class HomepageHero extends Component {
         >
           <div style={{ width: "50%", paddingTop: "70px" }}>
             <h1 className="h1">Prill.Tech</h1>
+            <p style={{ color: "white" }}>Inventing the future.</p>
             <Button type={"Link"} to={"/testpage"} style={{ width: "400px" }}>
               Click to go to TestPage
             </Button>
