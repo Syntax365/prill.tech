76220c6dd159e2b12fd16c4c63b1ab5b132721db
Syntax365 <synxtax365@gmail.com>
2021-04-10 11:48:08 -0400

    Init: p5.js SVG library

--- Client/src/Pages/Homepage/Hero/HomepageHero.jsx
+++ Client/src/Pages/Homepage/Hero/HomepageHero.jsx
@@ -8,14 +8,13 @@ class HomepageHero extends Component {
   render() {
     return (
       <>
-        <div id={`hero-img`} className={`hero`} />;
+        <div id={`hero-img`} className={``} />
         <div
           id={`hero-container`}
           style={{ position: "relative", zIndex: 10, width: "100%" }}
         >
           <div style={{ width: "50%", paddingTop: "70px" }}>
-            <h1 className="h1">Prill.Tech</h1>
-            <p style={{ color: "white" }}>Inventing the future.</p>
+            <h1 className="">Prill.Tech</h1>
             <Button type={"Link"} to={"/testpage"} style={{ width: "400px" }}>
               Click to go to TestPage
             </Button>
